9afc7d914d83c7a7ba287b0e5bbc13ea03312bc1
vergilx6x <med.work96@hotmail.com>
2024-11-19 18:15:45 +0100

    Task 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,35 +33,35 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
-        """
-        Adds a user to the database.
-
-        Args:
-        email (str): The email address of the user.
-        hashed_password (str): The hashed password of the user.
-
-        Returns:
-        User: The User object representing the newly added user.
-        """
-        session = self._session
-        user = User(email=email, hashed_password=hashed_password)
-        session.add(user)
-        session.commit()
-        session.refresh(user)
-        return user
-
     # def add_user(self, email: str, hashed_password: str) -> User:
-    #     """Adds a new user to the database.
     #     """
-    #     try:
-    #         new_user = User(email=email, hashed_password=hashed_password)
-    #         self._session.add(new_user)
-    #         self._session.commit()
-    #     except Exception:
-    #         self._session.rollback()
-    #         new_user = None
-    #     return new_user
+    #     Adds a user to the database.
+
+    #     Args:
+    #     email (str): The email address of the user.
+    #     hashed_password (str): The hashed password of the user.
+
+    #     Returns:
+    #     User: The User object representing the newly added user.
+    #     """
+    #     session = self._session
+    #     user = User(email=email, hashed_password=hashed_password)
+    #     session.add(user)
+    #     session.commit()
+    #     session.refresh(user)
+    #     return user
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Adds a new user to the database.
+        """
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
+        return new_user
 
     def find_user_by(self, **kwargs) -> User:
 
