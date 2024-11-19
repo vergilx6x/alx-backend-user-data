30a9a322b6c50a4ad32920f12ac99bdf5732fe91
vergilx6x <med.work96@hotmail.com>
2024-11-19 18:15:00 +0100

    Task 1

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
+from user import Base
+from user import User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        Adds a user to the database.
+
+        Args:
+        email (str): The email address of the user.
+        hashed_password (str): The hashed password of the user.
+
+        Returns:
+        User: The User object representing the newly added user.
+        """
+        session = self._session
+        user = User(email=email, hashed_password=hashed_password)
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+        return user
+
+    # def add_user(self, email: str, hashed_password: str) -> User:
+    #     """Adds a new user to the database.
+    #     """
+    #     try:
+    #         new_user = User(email=email, hashed_password=hashed_password)
+    #         self._session.add(new_user)
+    #         self._session.commit()
+    #     except Exception:
+    #         self._session.rollback()
+    #         new_user = None
+    #     return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+
+        return
