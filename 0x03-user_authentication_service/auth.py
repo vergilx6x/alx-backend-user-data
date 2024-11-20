fb7e8cd7ca4981a9743bce58ff40140db87b0373
vergilx6x <med.work96@hotmail.com>
2024-11-20 19:04:31 +0100

    Task 2

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,17 +3,10 @@
 import bcrypt
 
 
-def _hash_password(self, password: str) -> bytes:
+def _hash_password(password: str) -> bytes:
     """ This method takes a 'password' string argument,
     and hashing it using 'bycrypt.hashpw',
     returns bytes"""
     password = password.encode('utf-8')
     salt = bcrypt.gensalt()  # Generate a random salt
     return bcrypt.hashpw(password, salt)
-
-# password = "my_secure_password".encode('utf-8')  # Convert to bytes
-# salt = bcrypt.gensalt()  # Generate a random salt
-# hashed = bcrypt.hashpw(password, salt)
-# print(hashed)
-
-_hash_password(password='Testpwd')
