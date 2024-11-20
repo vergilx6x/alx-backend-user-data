156ec20d801e6c17dbe98e863d17a34a63f0044b
vergilx6x <med.work96@hotmail.com>
2024-11-20 20:00:13 +0100

    adding a basic flaks app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Basic flask app with a signle route:
+"GET - "/" route."""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """ Simple flaks route that return a string."""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
