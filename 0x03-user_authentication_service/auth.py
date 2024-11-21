dbbb4366729b1f188d97b3ce45597cea38627278
vergilx6x <med.work96@hotmail.com>
2024-11-21 17:30:12 +0100

    Fixing the register_user method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -36,12 +36,11 @@ class Auth:
         and register a user by adding them into the database"""
         try:
             user = self._db.find_user_by(email=email)
-            raise ValueError(f"User with email '{email}' already exists.")
 
         except NoResultFound:
             password = _hash_password(password)
-            user = self._db.add_user(email, password)
-            return user
+            return self._db.add_user(email, password)
+        raise ValueError("User {} already exists".format(email))
 
     def valid_login(self, email: str, password) -> bool:
         """ Expects two arguments:
@@ -52,7 +51,7 @@ class Auth:
         user = None
         try:
             user = self._db.find_user_by(email=email)
-            if user:
+            if user is not None:
                 return bcrypt.checkpw(
                     password.encode('utf-8'),
                     user.hashed_password
