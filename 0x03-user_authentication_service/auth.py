681d7b4a131869310ab7113205dd8c492ac4cb20
vergilx6x <med.work96@hotmail.com>
2024-11-21 20:09:32 +0100

    Mandatory tasks, and cleaning up the project

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ from db import DB
 from db import User
 from sqlalchemy.orm.exc import NoResultFound
 from uuid import uuid4
+from typing import Union
 
 
 def _hash_password(password: str) -> bytes:
@@ -73,7 +74,7 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
         return session_id
 
-    def get_user_from_session_id(self, session_id: str) -> User | None:
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         """Takes a single argument "email",
         and return the corresponding User or None
         """
