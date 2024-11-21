14bc5bf0631d43994d2015ae8638f801b4aec257
vergilx6x <med.work96@hotmail.com>
2024-11-21 19:58:51 +0100

    Fixing the users flask method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,7 +73,7 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
         return session_id
 
-    def get_user_from_session_id(self, session_id: str) -> User | None:
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         """Takes a single argument "email",
         and return the corresponding User or None
         """
