52f4decddce0b6aa36bb3fc09d5123c2e89c779e
vergilx6x <med.work96@hotmail.com>
2024-11-21 17:37:52 +0100

    Fixing the users flask method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
 """ Basic flask app with a signle route:
 "GET - "/" route."""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index() -> str:
     """ Simple flaks route that return a string."""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users/", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """ Expects two form data fields
+    "email: user email,
+    password: user password,
+    return a JSON payload."""
+    email, password = request.form.get("email"), request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
