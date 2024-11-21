1adb2ef70c1b18c27d80a0e3ba0f1c782214a93e
vergilx6x <med.work96@hotmail.com>
2024-11-21 20:02:54 +0100

    Fixing the users flask method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -27,6 +27,7 @@ def users() -> str:
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
+
 @app.route("/sessions/", methods=["POST"], strict_slashes=False)
 def login() -> str:
     """ Creates a new session for an User,
